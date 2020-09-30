93260d36cabaab4a5bb0ddf320899594e39ec566
matrev <matrev@DESKTOP-MATREV.localdomain>
2020-09-30 08:42:13 -0500

    Updates

---
+++ precache-manifest.4e77da74b4a4481654acaff320f1bdcb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b70f4e9b4bf496a3907a44d506e7c15",
+    "url": "/brothersistershow.github.io/index.html"
+  },
+  {
+    "revision": "9685a5ccff43ba69c77f",
+    "url": "/brothersistershow.github.io/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "28a31f836648237238df",
+    "url": "/brothersistershow.github.io/static/js/2.66de64bc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/brothersistershow.github.io/static/js/2.66de64bc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9685a5ccff43ba69c77f",
+    "url": "/brothersistershow.github.io/static/js/main.2465ed0a.chunk.js"
+  },
+  {
+    "revision": "9131cfabb31d440fdfe4",
+    "url": "/brothersistershow.github.io/static/js/runtime-main.0cbc9749.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/brothersistershow.github.io/static/media/logo.5d5d9eef.svg"
+  }
+]);
