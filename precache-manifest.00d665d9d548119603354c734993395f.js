4a7b3bb162beb7c2b07d5abafded0c6000c8312b
matrev <matrev@DESKTOP-MATREV.localdomain>
2020-09-30 08:51:33 -0500

    Updates

---
+++ precache-manifest.00d665d9d548119603354c734993395f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "620e60140595d2e7015ccc8c91d2d565",
+    "url": "/index.html"
+  },
+  {
+    "revision": "786f48d929ad69155865",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "28a31f836648237238df",
+    "url": "/static/js/2.66de64bc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.66de64bc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "786f48d929ad69155865",
+    "url": "/static/js/main.b2cdeeb6.chunk.js"
+  },
+  {
+    "revision": "2f00e27ee82fc3407f60",
+    "url": "/static/js/runtime-main.8e50440a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
