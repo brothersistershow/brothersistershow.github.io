c0fd51dcdb490641c4039cb2aafb4def1429ac8d
matrev <markanthonytrevinojr@gmail.com>
2020-12-16 23:07:06 -0600

    Updates

---
+++ precache-manifest.df97e95a37fabd2ddc8f308a4e94150f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "920468de87b0564e51bf311f46573d31",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2e5de23c01886e97df84",
+    "url": "/static/css/2.6414bb7b.chunk.css"
+  },
+  {
+    "revision": "239e62047509f20703a7",
+    "url": "/static/css/main.3632a36d.chunk.css"
+  },
+  {
+    "revision": "2e5de23c01886e97df84",
+    "url": "/static/js/2.91de2b80.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.91de2b80.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "239e62047509f20703a7",
+    "url": "/static/js/main.2b41f50e.chunk.js"
+  },
+  {
+    "revision": "2f00e27ee82fc3407f60",
+    "url": "/static/js/runtime-main.8e50440a.js"
+  },
+  {
+    "revision": "99fa4d4ea6d3ee5241182877054cee53",
+    "url": "/static/media/logo.99fa4d4e.PNG"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.d2da9a49de0a0b6604ab01fb79293455.js"
+  "/precache-manifest.df97e95a37fabd2ddc8f308a4e94150f.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
